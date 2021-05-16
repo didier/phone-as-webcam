462b770700556b89aedeb551055320a51e663872
didier <didierlennon@gmail.com>
2021-05-16 16:12:56 +0200

    Update styling

---
+++ README.md
@@ -0,0 +1,3 @@
+# Use your smartphone camera as a webcam, via OBS Virtual Camera.
+
+Main inspiration taken from https://github.com/apancik/iphone-as-a-webcam.
--- main.js
+++ main.js
@@ -1,6 +1,59 @@
-import './style.css'
+import './style.css';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+// DOM Elements
+const video = document.querySelector('video');
+const backCamera = document.querySelector('.back');
+const frontCamera = document.querySelector('.front');
+const fullscreen = document.querySelector('.fullscreen');
+
+/**
+ *
+ * @param {MediaProvider} stream The video stream to render
+ */
+function render(stream) {
+  video.srcObject = stream;
+}
+
+/**
+ * Gets the corresponding device camera based on a direction.
+ *
+ * @param {string} camera The device camera to use
+ * @returns {ConstrainDOMString} The cross-platform camera `facingMode`
+ */
+function getFacingMode(camera) {
+  switch (camera) {
+    case 'front':
+      return 'user';
+    case 'back':
+      return 'environment';
+    default:
+      return 'environment';
+  }
+}
+
+/**
+ * Captures output of a specified device camera.
+ *
+ * @param {string} camera The device camera to use
+ */
+function capture(camera) {
+  navigator.mediaDevices
+    .getUserMedia({
+      video: { facingMode: getFacingMode(camera) },
+    })
+    .then(render)
+    .catch(console.error);
+}
+
+/**
+ * Cross-platform way to make a video enter fullscreen.
+ */
+const enterFullScreen =
+  video.webkitEnterFullScreen ||
+  video.requestFullscreen ||
+  video.webkitRequestFullscreen ||
+  video.msRequestFullscreen;
+
+frontCamera.addEventListener('click', () => capture('front'));
+backCamera.addEventListener('click', () => capture('back'));
+fullscreen.addEventListener('click', enterFullScreen);
